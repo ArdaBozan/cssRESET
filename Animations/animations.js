df841248bace6c1932a0b1d2736ff3bf4bd55352
Arda Bozan <bozanarda9@gmail.com>
2025-02-06 12:51:20 +0300

    d

--- Animations/animations.js
+++ Animations/animations.js
@@ -1,12 +1,4 @@
 /*--------------------------------------------- SCROLL ANIMATIONS (WITH REPEAT SUPPORT) --------------------------------------------------*/
-const animations = [
-    { class: 'add-text-animation', animation: 'text-animation' },
-    { class: 'add-x-toRight-animation', animation: 'x-toRight-animation' },
-    { class: 'add-x-toLeft-animation', animation: 'x-toLeft-animation' },
-    { class: 'add-scale-animation', animation: 'scale-animation' },
-    { class: 'add-opacity-animation', animation: 'opacity-animation' }
-    // Add more animations here (With usings css [Line 1120])
-];
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
