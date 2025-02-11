74f48558ae48131b6d69d4cf8ac1f71b5ec65610
Arda Bozan <bozanarda9@gmail.com>
2025-02-11 18:35:54 +0300

    Update smooth-slider.js

--- Sliders/smooth-slider.js
+++ Sliders/smooth-slider.js
@@ -1,65 +1,46 @@
-document.querySelectorAll('.smooth-slider-container').forEach((slider) => {
-    let isDragging = false;
-    let startX, scrollLeft;
-    let velocity = 0;
-    let lastX, lastTime;
-    let momentumID;
-  
-    const startDragging = (pageX) => {
-      isDragging = true;
-      slider.classList.add('grabbing');
-      startX = pageX;
-      scrollLeft = slider.scrollLeft;
-      velocity = 0;
-      lastX = pageX;
-      lastTime = Date.now();
-      cancelMomentumScroll();
-    };
-  
-    const stopDragging = () => {
-      isDragging = false;
-      slider.classList.remove('grabbing');
-      beginMomentumScroll();
-    };
-  
-    const onDrag = (pageX) => {
-      if (!isDragging) return;
-      const deltaX = pageX - startX;
-      slider.scrollLeft = scrollLeft - deltaX;
-  
-      // Hızı hesapla
-      const now = Date.now();
-      const deltaTime = now - lastTime;
-      velocity = (pageX - lastX) / deltaTime;
-      lastX = pageX;
-      lastTime = now;
-    };
-  
-    const beginMomentumScroll = () => {
-      cancelMomentumScroll();
-      momentumID = requestAnimationFrame(momentumScroll);
-    };
-  
-    const cancelMomentumScroll = () => {
-      cancelAnimationFrame(momentumID);
-    };
-  
-    const momentumScroll = () => {
-      if (Math.abs(velocity) < 0.01) return;
-      slider.scrollLeft -= velocity * 50; // Kaydırma miktarını hızla çarpıyoruz
-      velocity *= 0.95; // Sürtünme etkisi
-      momentumID = requestAnimationFrame(momentumScroll);
-    };
-  
-    // Mouse Olayları
-    slider.addEventListener('mousedown', (e) => startDragging(e.pageX));
-    slider.addEventListener('mouseup', stopDragging);
-    slider.addEventListener('mouseleave', stopDragging);
-    slider.addEventListener('mousemove', (e) => onDrag(e.pageX));
-  
-    // Dokunmatik Olaylar
-    slider.addEventListener('touchstart', (e) => startDragging(e.touches[0].pageX));
-    slider.addEventListener('touchend', stopDragging);
-    slider.addEventListener('touchmove', (e) => onDrag(e.touches[0].pageX));
-  });
-  
+const smoothSlider = document.querySelector('.smooth-slider-container');
+
+let isDragging = false;
+let startX, scrollLeft;
+
+smoothSlider.addEventListener('mousedown', (e) => {
+  isDragging = true;
+  smoothSlider.classList.add('grabbing');
+  startX = e.pageX - smoothSlider.offsetLeft;
+  scrollLeft = smoothSlider.scrollLeft;
+});
+
+smoothSlider.addEventListener('mouseleave', () => {
+  isDragging = false;
+  smoothSlider.classList.remove('grabbing');
+});
+
+smoothSlider.addEventListener('mouseup', () => {
+  isDragging = false;
+  smoothSlider.classList.remove('grabbing');
+});
+
+smoothSlider.addEventListener('mousemove', (e) => {
+  if (!isDragging) return;
+  e.preventDefault();
+  const x = e.pageX - smoothSlider.offsetLeft;
+  const walk = (x - startX) * 2;
+  smoothSlider.scrollLeft = scrollLeft - walk;
+});
+
+smoothSlider.addEventListener('touchstart', (e) => {
+  isDragging = true;
+  startX = e.touches[0].pageX - smoothSlider.offsetLeft;
+  scrollLeft = smoothSlider.scrollLeft;
+});
+
+smoothSlider.addEventListener('touchend', () => {
+  isDragging = false;
+});
+
+smoothSlider.addEventListener('touchmove', (e) => {
+  if (!isDragging) return;
+  const x = e.touches[0].pageX - smoothSlider.offsetLeft;
+  const walk = (x - startX) * 2;
+  smoothSlider.scrollLeft = scrollLeft - walk;
+});
